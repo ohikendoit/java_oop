9f9fddc6153598d218bc9985b12de4e39c9fdbaf
Yoonseong Jung <ohikendoit@gmail.com>
2023-01-30 09:49:55 +0900

    Create Address.java

---
+++ address_book/Address.java
@@ -0,0 +1,33 @@
+package address_book;
+
+
+/**
+ * 주소 관리 프로그램
+ * @author 211-13
+ *
+ */
+public class Address {
+	
+	private String name;
+	private String phoneNumber;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
