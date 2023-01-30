5704ca4787bf1676050dc45946939fd2539503bf
Yoonseong Jung <ohikendoit@gmail.com>
2023-01-30 09:48:17 +0900

    Create BoardVO.java

---
+++ board/BoardVO.java
@@ -0,0 +1,36 @@
+package board;
+
+public class BoardVO {
+	
+	private String subject;
+	private String writer;
+	private int number;
+	private int readCount;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getReadCount() {
+		return readCount;
+	}
+	public void setReadCount(int readCount) {
+		this.readCount = readCount;
+	}
+	
+	
+}
