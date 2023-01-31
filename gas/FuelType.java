352703d5263bd7e986798867a37f6b5c4a2bf65f
Yoonseong Jung <ohikendoit@gmail.com>
2023-01-31 17:00:17 +0900

    Create FuelType.java

---
+++ gas/FuelType.java
@@ -0,0 +1,22 @@
+package gas;
+
+/**
+ * 연료의 종류
+ * @author 211-13
+ *
+ */
+public enum FuelType {
+	//싱글톤타입: 유일한 값을 가지는 객체들
+	/**
+	 * 휘발유
+	 */
+	GASOLIN,
+	/**
+	 * 경유
+	 */
+	DIESEL,
+	/**
+	 * LPG
+	 */
+	LPG;
+}
